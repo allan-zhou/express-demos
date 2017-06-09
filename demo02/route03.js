7324b4fe003133c50f849a85fe5e1708f90ea6e1
周佳良 <izhoujialiang@qq.com>
2017-06-09 22:04:08 +0800

    路由路径方式：字符串

--- demo02/route03.js
+++ demo02/route03.js
@@ -1,11 +1,22 @@
-let express = require('express');
-// import AppRoute from './routes/AppRoute';
-// import UserRoute from './routes/UserRoute';
-// import BlogRoute from './routes/BlogRoute';
-let AppRoute = require('./routes/AppRoute');
+var express = require('express');
+var app = express();
 
-let app = express();
+//路由路径方式：字符串
 
-app.use('/demo', AppRoute);
+//匹配根路由
+app.get('/',(req, res)=>{
+    res.send('root');
+});
+
+//匹配 /about 
+app.get('/about',(req, res) => {
+    res.send('about');
+});
+
+//匹配 /random.text
+app.get('/random.text',(req, res) => {
+    res.send('random.text');    
+});
+
 
 app.listen(8080);
